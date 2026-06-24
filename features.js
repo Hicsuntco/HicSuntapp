7397b5e7e3aee0e0cd3128c885a6c0c83c565c74
Hic Sunt Deploy <hicsunt@deploy.com>
2026-06-24 05:07:02 +0000

    fix: bouton 'Voir les hébergements' du budget ouvre la liste complète (openAllBookings) au lieu du détail d'un seul hébergement

--- features.js
+++ features.js
@@ -237,7 +237,7 @@ function budgetView(){
     + (isGen
       ? '<div class="ov-foot"><div class="foot-price">'
         + '<div><div class="fp-v">' + eur(b.total) + '</div><div class="fp-l">estimation totale</div></div>'
-        + '<button class="btn" onclick="openBooking(\'' + (ITINERARY.accommodations[0]?ITINERARY.accommodations[0].id:'') + '\')">Voir les hébergements</button>'
+        + '<button class="btn" onclick="if(window.openAllBookings){openAllBookings()}else{openBooking(\'' + (ITINERARY.accommodations[0]?ITINERARY.accommodations[0].id:'') + '\')}">Voir les hébergements</button>'
         + '</div></div>'
       : '<div class="ov-foot"><div class="foot-price">'
         + '<div><div class="fp-v">' + eur(rest) + '</div><div class="fp-l">solde restant</div></div>'
